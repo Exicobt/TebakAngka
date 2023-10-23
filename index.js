f5bf29a9a13b7fd7a1cdbc2e6603c019d419f859
Exicobt <naufalalfatihamriansyah@gmail.com>
2023-10-23 19:28:43 +0700

    update

--- index.js
+++ index.js
@@ -4,55 +4,36 @@ const result = document.querySelector(".result");
 const guess = document.querySelector(".guess");
 const waktu = document.querySelector(".timer");
 
-const num = [];
-
-for (let i = 1; i <= 100; i++) {
-  num.push(i);
-}
-
-const randomNum = Math.floor(Math.random() * num.length);
-const GETNum = num[randomNum];
-
-tebak.addEventListener("click", () => {
-  if (guess.value == GETNum) {
-    result.style.color = "#33ff33";
-    result.innerHTML = `Tebakan mu Benarr!!!`;
-  } else if (guess.value > GETNum + 3 && guess.value <= GETNum + 10) {
-    result.innerHTML = "Ayo Semangat!!! Sudah mau dekat";
-  } else if (guess.value < GETNum - 3 && guess.value >= GETNum - 10) {
-    result.innerHTML = "Ayo Semangat!!! Sudah mau dekat";
-  } else if (guess.value <= GETNum + 2 && guess.value > GETNum) {
-    result.innerHTML = "Sedikit lagii...";
-  } else if (guess.value >= GETNum - 2 && guess.value < GETNum) {
-    result.innerHTML = "Sedikit lagii...";
-  } else if (guess.value > GETNum + 10) {
-    result.innerHTML = "Angka mu terlalu besar!!";
-  } else if (guess.value < GETNum - 10) {
-    result.innerHTML = "Angka mu terlalu kecil!!";
-  }
-});
-
-tebak.addEventListener("click", () => {
-  if (guess.value == GETNum) {
-    guess.value;
-    clearInterval(interval);
-    waktu.innerHTML = ''
-  } else {
-    guess.value = "";
-  }
-});
-
-  let time = 30;
-  const interval = setInterval(function timer() {
-    time--;
-    waktu.innerHTML = time + "s";
-
-    if (time === 0) {
-      result.innerHTML = `GAME OVER!! Angka nya adalah ${GETNum}`;
+const randomNum = Math.floor(Math.random() * (100) + 1);
+let time = 30;
+const interval = setInterval(() => {
+  time--;
+  waktu.innerHTML = time + "s";
+  tebak.addEventListener("click", () => {
+    if (time > 0) {
+      if (guess.value == randomNum) {
+        result.style.color = "#33ff33";
+        result.innerHTML = `Tebakan mu Benarr!!!`;
+        waktu.innerHTML = ''
+        clearInterval(interval)
+      } else if (guess.value > randomNum + 3 && guess.value <= randomNum + 10) {
+        result.innerHTML = "Ayo Semangat!!! Sudah mau dekat";
+      } else if (guess.value < randomNum - 3 && guess.value >= randomNum - 10) {
+        result.innerHTML = "Ayo Semangat!!! Sudah mau dekat";
+      } else if (guess.value <= randomNum + 2 && guess.value > randomNum) {
+        result.innerHTML = "Sedikit lagii...";
+      } else if (guess.value >= randomNum - 2 && guess.value < randomNum) {
+        result.innerHTML = "Sedikit lagii...";
+      } else if (guess.value > randomNum + 10) {
+        result.innerHTML = "Angka mu terlalu besar!!";
+      } else if (guess.value < randomNum - 10) {
+        result.innerHTML = "Angka mu terlalu kecil!!";
+      }
+    }
+  });
+    if (time == 0) {
+      result.innerHTML = `GAME OVER!! Angka nya adalah ${randomNum}`;
       result.style.color = "red";
       clearInterval(interval);
     }
-  }, 1000);
-
-  
-
+}, 1000);
